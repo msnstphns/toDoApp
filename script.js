d8bef01c7ddde2db34d9d923bb0f01f1f9f5d598
msnstphns <mason.a.stephens@gmail.com>
2024-03-01 16:25:44 +0000

    testing

--- script.js
+++ script.js
@@ -8,30 +8,4 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = input.value;
     listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-  }
-  input.value = "";
-  saveTask();
-}
-
-listContainer.addEventListener("click", function(e) {
-  if(e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
-    saveTask();
-} else if (e.target.tagName === "SPAN") {
-  e.target.parentElement.remove();
-  saveTask()
-}
-}, false);
-
-function saveTask() {
-  localStorage.setItem("task", listContainer.innerHTML);
-}
-
-function viewTask() {
-  listContainer.innerHTML = localStorage.getItem("task");
-}
-
-viewTask();
+    le
